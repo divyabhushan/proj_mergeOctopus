58b897b6b433d1402364cc11b1134117d432cb88
Tim Pettersen <tim@atlassian.com>
2015-01-16 10:06:15 -0800

    Make each leg three commits long

--- build-octopus.sh
+++ build-octopus.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 for i in `seq 1 8`; do
-    git checkout -b leg$i
-    echo leg$i > leg$i
-    git add .
-    git commit -m "Leg $i"
+    git checkout -b "leg-$i"
+    for j in `seq 1 3`; do
+        echo "commit $j" >> "leg$i"
+        git add .
+        git commit -m "Leg $i commit $j"
+    done
     git checkout master
 done
