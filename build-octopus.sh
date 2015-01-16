9d8ae9928b915688b1ef195d94c4f3bb7a261364
Tim Pettersen <tim@atlassian.com>
2015-01-16 10:09:26 -0800

    Merge legs together

--- build-octopus.sh
+++ build-octopus.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+merge_cmd = "git merge "
 for i in `seq 1 8`; do
     git checkout -b "leg-$i"
     for j in `seq 1 3`; do
@@ -6,5 +7,7 @@ for i in `seq 1 8`; do
         git add .
         git commit -m "Leg $i commit $j"
     done
+    merge_cmd = "$merge_cmd leg-$i"
     git checkout master
 done
+eval $merge_cmd
